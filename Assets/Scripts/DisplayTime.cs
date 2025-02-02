5dae9df10d261c0c10f3b5252df75bec92527457
TiagoANeto <tiago98.ta@gmail.com>
2025-02-01 23:56:40 -0300

    adicionando display de hora

---
+++ Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayTime : MonoBehaviour
+{
+    public GameObject displayHour;
+    public int hour;
+    public int minutes;
+
+    void Update()
+    {
+        hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;
+        displayHour.GetComponent<Text>().text = " " + hour + " : " + minutes;
+    }
+}
