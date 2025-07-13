6d584e97e030a5209b09a88de1ed1aef091031bc
TiagoANeto <tiago98.ta@gmail.com>
2025-07-13 02:26:55 -0300

    New Assets, new post processing effect

--- Assets/Scripts/ToDoList/ToDoListInterface.cs
+++ Assets/Scripts/ToDoList/ToDoListInterface.cs
@@ -6,13 +6,11 @@ using TMPro;
 
 public class ToDoListInterface : MonoBehaviour
 {
-   [Header("Panel de grid onde seram instanciadas as tasks")]
-   [Space(10)]
+   [Header("Panel de grid onde seram instanciadas as tasks")] [Space(10)]
    public GameObject grid;
    private int countInstance = 0;
 
-   [Header("Task prefab")]
-   [Space(10)]
+   [Header("Task prefab")] [Space(10)]
    public GameObject taskAddPrefab;
 
    public void AddTask() // metodo para instanciar o prefab que contem as informações de task
